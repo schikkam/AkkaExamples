357d3a013b71212dddd1dc59ab4aa4a6d3abd08b
Shiva <shiva.chikkam@gmail.com>
2015-12-24 19:54:28 +0530

    cleanup

--- src/main/scala/com/avis/stream/Foo.scala
+++
@@ -1,7 +0,0 @@
-package com.avis.stream
-
-object Foo {
-  val onEvent = (topic: String, payload: Any) => Some(topic) collect {
-    case "Purchase" => println("Foo received: Purchase = " + topic + ", payload = " + payload)
-  }
-}
